df2b40c08cb36fd570d21e4739c7d71ce2afc85a
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-02 20:25:59 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# for building
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+ALLOW_MISSING_DEPENDENCIES := true
+
 DEVICE_PATH := device/realme/RMX5070
 
 # Architecture
@@ -12,6 +16,7 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
+
 # 64-bit support
 TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_SUPPORTS_32_BIT_APPS := true
