cafe18b1c4f2ed44d90eab2eaf23d63484f76fe7
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-04 22:57:02 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,4 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX5070.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RMX5070-eng
+    twrp__RMX5070-user \
+    twrp__RMX5070-userdebug \
+    twrp__RMX5070-eng
