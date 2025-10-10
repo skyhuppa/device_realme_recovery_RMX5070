8b16353c4032db7276a0e81ab7fc731a849e0f2b
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-10 18:45:05 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,9 +93,9 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_DTB_SIZE := 4016217
 BOARD_DTB_OFFSET := 0x01f00000
 BOARD_VENDOR_BASE := 0x00000000
-# BOARD_VENDOR_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 log_buf_len=2M nosoftlockup bootconfig # (default cmdline)
-BOARD_VENDOR_CMDLINE += "video=vfb:640x400,bpp=32,memsize=3072000 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image bootconfig loop.max_part=7 androidboot.selinux=permissive"
-BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
+BOARD_VENDOR_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 log_buf_len=2M nosoftlockup # bootconfig  (default cmdline)
+# BOARD_VENDOR_CMDLINE += "video=vfb:640x400,bpp=32,memsize=3072000 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image bootconfig loop.max_part=7 androidboot.selinux=permissive"
+# BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += bootconfig
 
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
