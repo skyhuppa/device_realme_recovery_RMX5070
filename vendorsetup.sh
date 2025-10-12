72c2b75874f7d7004ae53eb6104892f538be11ad
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-12 18:25:32 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,11 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-export ALLOW_MISSING_DEPENDENCIES=true
+# Orangefox Flags
+export ALLOW_MISSING_DEPENDANCIES=true
+export TARGET_ARCH=arm64
+export OF_FLASHLIGHT_ENABLE=1
+export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
+export OF_MAINTAINER="Jamie_Naden_Maxim_Archer_Ahmed_Carlo | pa1q"
+export TARGET_DEVICE_ALT="pa1q"
+export OF_ENABLE_LPTOOLS=1
+export OF_USE_LEGACY_BATTERY_SERVICES=1
+export OF_SCREEN_H=2340
+export FOX_MAINTAINER_PATCH_VERSION="0"
