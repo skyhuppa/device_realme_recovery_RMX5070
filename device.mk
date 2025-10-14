a7bd36e699dfc836efecaa4ebba0161bcc352fff
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-14 01:10:03 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -57,10 +57,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Host tools to install
-PRODUCT_HOST_PACKAGES += \
-    atest    
-
 PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/RMX5070/prebuilt/modules:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/lib/modules \
-    $(OUT_DIR)/target/product/RMX5070/prebuilt/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/root/first_stage_ramdisk/fstab.qcom
+    $(OUT_DIR)/target/product/RMX5070/prebuilt/modules:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules \
+    $(OUT_DIR)/target/product/RMX5070/prebuilt/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
