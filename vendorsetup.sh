24a407a36878ad5058d5dbe5d62eb8a048b2e4b9
Adasi Kaakyire <Skyhuppa@gmail.com>
2025-10-22 18:51:04 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,1 @@
-# Orangefox Flags
 export ALLOW_MISSING_DEPENDANCIES=true
-export TARGET_ARCH=arm64
-export OF_FLASHLIGHT_ENABLE=1
-export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
-export OF_MAINTAINER="Pain | RMX5070"
-export TARGET_DEVICE_ALT="RMX5070N"
-export OF_ENABLE_LPTOOLS=1
-export OF_USE_LEGACY_BATTERY_SERVICES=1
-export OF_SCREEN_H=2400
-export FOX_MAINTAINER_PATCH_VERSION="0"
