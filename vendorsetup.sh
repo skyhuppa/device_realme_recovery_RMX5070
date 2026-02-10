1fb153fa96f06dae8deb706eb37766b31436154d
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-10 22:02:50 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,7 +17,7 @@
 #
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="RMX3085"
+FDEVICE="RMX5070"
 #set -o xtrace
 
 fox_get_target_device() {
@@ -40,67 +40,78 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
  	export ALLOW_MISSING_DEPENDENCIES=true
   export TARGET_DEVICE_ALT="RMX5070"
 #	export FOX_TARGET_DEVICES="RMX5070"
-#	export FOX_VIRTUAL_AB_DEVICE=1
-#	export FOX_AB_DEVICE=1
-  
-	# export OF_KEEP_FORCED_ENCRYPTION=1
-    export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-	export OF_SKIP_FBE_DECRYPTION_SDKVERSION=34 # Don't try to decrypt A14(?)
-	export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
-	export OF_PATCH_AVB20=1
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=100
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-    export OF_HIDE_NOTCH=1
-	export OF_CLOCK_POS=1
-    export OF_ENABLE_LPTOOLS=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
-    export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800" # Tue Jan 1 2019 00:00:00 GMT
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	# export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_USE_GREEN_LED=
-    export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.mt6785.rc for more information
+export OF_DEFAULT_KEYMASTER_VERSION="4"
 
-        # Device-specific flags
-    export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+# Version & Variant
+export FOX_BUILD_TYPE=Stable
+export FOX_MAINTAINER_PATCH_VERSION=1
+export USE_CCACHE="1"
+export TARGET_ARCH="arm64"
+export FOX_VANILLA_BUILD="1"
+export OF_MAINTAINER="skyhuppa"
+export LC_ALL="C"
+export FOX_VANILLA_BUILD=1
+export FOX_VIRTUAL_AB_DEVICE=1
+export FOX_VENDOR_BOOT_RECOVERY=1
+export FOX_NO_SAMSUNG_SPECIAL=1
+export FOX_INSTALLER_VENDOR_BOOT_RAMDISK_INSTALL=0
+export OF_FLASHLIGHT_ENABLE=1
+export FOX_DELETE_AROMAFM=1
+export FOX_USE_UPDATED_MAGISKBOOT=1
 
-	# use magisk 21.4 for the magisk addon
-	# export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+# OrangeFox Addons
+export FOX_ENABLE_APP_MANAGER=1
 
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	# export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
+# Binaries & Tools
+export FOX_ENABLE_KERNELSU_SUPPORT=1
+export FOX_ENABLE_KERNELSU_NEXT_SUPPORT=1
+export FOX_ENABLE_SUKISU_SUPPORT=1
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
+export FOX_REPLACE_TOOLBOX_GETPROP=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
 
-	# OTA for custom ROMs
-	# export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+# Compression & Binary
+export OF_USE_LZ4_COMPRESSION=1
+export FOX_USE_LZ4_BINARY=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 
-	# -- add settings for R11 --
-    export OF_MAINTAINER=Skyhuppa
-    export FOX_BUILD_TYPE="Stable"
-	export FOX_VERSION="R11.3"
-    export FOX_VARIANT=A12.1
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	# -- end R11 settings --
+# OrangeFox Device Properties
+export FOX_AB_DEVICE=1
+export OF_DEVICE_WITHOUT_PERSIST=1
+
+# Others
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export OF_SCREEN_H=2340
+export OF_STATUS_H=109
+export OF_STATUS_INDENT_LEFT=64
+export OF_STATUS_INDENT_RIGHT=64
+export OF_HIDE_NOTCH=1
+export OF_CLOCK_POS=1
+export OF_USE_GREEN_LED=0
+export OF_ENABLE_FRP_ADDON=1
+
+# Flashlight
+export OF_FL_PATH1="/tmp/flashlight"
+
+# Decrypt/Encrypt
+export OF_SKIP_FBE_DECRYPTION=1
+export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+
+# Device-specific flags
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/by-name/recovery"
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+export FOX_RECOVERY_BOOT_PARTITION="/dev/block/by-name/boot"
+
+# use magisk 21.4 for the magisk addon
+# export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
@@ -110,4 +121,3 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
 	fi
 fi
-#
