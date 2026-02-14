985b2a9e6c19b336d92dd954c1fcd611da21f89c
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-14 07:09:13 +0000

    Update twrp_RMX5070.mk

--- twrp_RMX5070.mk
+++ twrp_RMX5070.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := RMX5070
+# PRODUCT_RELEASE_NAME := RMX5070
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
@@ -7,24 +7,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from device
-$(call inherit-product, device/realme/$(PRODUCT_RELEASE_NAME)/device.mk)
+$(call inherit-product, device/realme/RMX5070/device.mk)
 
 # Assert
 # TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/realme/$(PRODUCT_RELEASE_NAME)/recovery/root,recovery/root)
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/realme/RMX5070/recovery/root,recovery/root)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+# PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
  	 $(LOCAL_PATH)/prebuilt/dtb:dtb.img
 
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := RMX5070
+PRODUCT_NAME := twrp_RMX5070
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme P3
 PRODUCT_MANUFACTURER := realme
