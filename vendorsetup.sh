006c3c5ad8660ddb709df07c52369f863bf99706
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-15 09:41:06 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,8 @@
-soong_namespace {
-}
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+export ALLOW_MISSING_DEPENDENCIES=true
