681a1a8334a36bbba79b6823cb8e74201c8954a2
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-09-01 19:31:51 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,10 +1,51 @@
-# API Level
-PRODUCT_SHIPPING_API_LEVEL := 34
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+AB_OTA_UPDATER := true
 
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libandroidicu
+AB_OTA_PARTITIONS += \
+    dtbo \
+    boot \
+    vendor_dlkm \
+    init_boot \
+    system \
+    product \
+    system_ext \
+    vbmeta \
+    system_dlkm \
+    vendor \
+    vbmeta_system \
+    odm \
+    vendor_boot
 
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libandroidicu.so
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    bootctrl.volcano.recovery
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,device/realme/pa1q/prebuilt/modules,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
+    $(LOCAL_PATH)/prebuilt/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
