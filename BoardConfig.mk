0ae9fef27a14b0c038a7ffc5ffb0246442726847
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-09-05 14:09:55 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,8 +53,8 @@ BOARD_DTB_OFFSET := 0x01f00000
 BOARD_VENDOR_BASE := 0x00000000
 BOARD_VENDOR_CMDLINE = video=vfb:640x400,bpp=32,memsize=3072000 log_buf_len=2M nosoftlockup bootconfig
 # BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 
-androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true 
-androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
+# androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true 
+# androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
 # BOARD_KERNEL_CMDLINE := bootconfig
 
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
