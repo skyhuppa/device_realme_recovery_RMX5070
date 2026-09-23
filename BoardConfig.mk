d32cd60d977cf6d1b5c25124f193edcbfa1c1dd2
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-09-23 23:39:16 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,9 +56,9 @@ BOARD_DTB_SIZE := 4488060
 BOARD_DTB_OFFSET := 0x01f00000
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_VENDOR_CMDLINE = video=vfb:640x400,bpp=32,memsize=3072000 log_buf_len=2M nosoftlockup bootconfig
-BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 
-androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true 
-androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
+# BOARD_BOOTCONFIG += androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 
+# androidboot.load_modules_parallel=false androidboot.hypervisor.protected_vm.supported=true 
+# androidboot.vendor.qspa=true androidboot.serialconsole=0 androidboot.selinux=permissive
 
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_MKBOOTIMG_ARGS += --kernel_cmdline $(BOARD_KERNEL_CMDLINE)
